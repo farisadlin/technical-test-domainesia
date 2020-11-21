3063c70880d580426c2967de42914e1e72ec0f9e
farisadlin <farisnura@gmail.com>
2020-11-21 21:51:42 +0700

    Adding clickable function on the logo and new favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,95 +1,97 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
-import Logo from '../images/white-logo.png';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Logo from "../images/white-logo.png";
 
-import { Button } from './Button';
+import { Button } from "./Button";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 1200) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
+  const showButton = () => {
+    if (window.innerWidth <= 1200) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    useEffect(() => {
-        showButton();
-      }, []);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    window.addEventListener('resize', showButton);
+  window.addEventListener("resize", showButton);
 
-    return (
-      <>
-        <nav className="navbar">
-          <div className="navbar-container">
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <a href="#hero-section">
             <img className="navbar-logo" src={Logo} alt="HostingKite"></img>
-            <div className="menu-icon" onClick={handleClick}>
-              <i className={click ? "fas fa-times" : "fas fa-bars"} />
-            </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <a
-                  href="#hero-section"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Home
-                </a>
-              </li>
-
-              <li className="nav-item">
-              <a
-                  href="#services-section"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Service
-                </a>
-              </li>
-
-              <li className="nav-item">
-              <a
-                  href="#testimonial-section"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Testimonial
-                </a>
-              </li>
-
-              <li className="nav-item">
-              <a
-                  href="#price-section"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Hosting
-                </a>
-              </li>
-
-              <li>
-              <a
-                  href="#"
-                  className="nav-links-mobile"
-                  onClick={closeMobileMenu}
-                >
-                  Sign In
-                </a>
-              </li>
-            </ul>
-            {button && <Button buttonStyle="btn--outline">SIGN IN</Button>}
+          </a>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-        </nav>
-      </>
-    );
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <a
+                href="#hero-section"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Home
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                href="#services-section"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Service
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                href="#testimonial-section"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Testimonial
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a
+                href="#price-section"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Hosting
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="nav-links-mobile"
+                onClick={closeMobileMenu}
+              >
+                Sign In
+              </a>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN IN</Button>}
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
