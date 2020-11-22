a88bc1d30ad5c4fc8f0f2a0ba398926058840e3a
farisadlin <farisnura@gmail.com>
2020-11-22 11:39:04 +0700

    Added some new UIs in Price Section

--- src/components/PriceSection.js
+++ src/components/PriceSection.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./PriceSection.css";
 
-function Price({data}) {
+function Price({ data }) {
   const [domain, setDomain] = useState(null);
   const [sshAccess, setSshAccess] = useState(null);
-  const [discount, setDiscount] = useState(true);
 
   useEffect(() => {
     setDomain(data.free_domain);
@@ -12,8 +11,95 @@ function Price({data}) {
   }, []);
 
   const formatPrice = (price) => {
-    return Intl.NumberFormat('id-ID').format(price)
+    return Intl.NumberFormat("id-ID").format(price);
+  };
+
+  const discountedPrice = (price) => {
+    const discount = 0.8;
+    return Intl.NumberFormat("id-ID").format(price / discount);
   }
+
+  /* Mengambil data JSON kemudian menaruh ke dalam 
+  masing-masing item gridbox */
+
+  const infoPrice = data.map((item, key) => {
+    const recommendedStyle =
+      item.hosting_type === "Super" ? (
+        <div className="recommended-price">
+          <p className="recommended-title">We Recommend</p>
+        </div>
+      ) : (
+        ""
+      );
+
+    const discountPrice = 
+      item.hosting_type === "Super" ? (
+        <div className="price-box">
+          <p className="title-price">
+            <span id="title-price-discount">{discountedPrice(item.price)}</span>
+          </p>
+          <div className="discount-price-container">
+            <sup id="discount-currency">Rp</sup>{" "}
+            <span id="title-price-detail">{formatPrice(item.price)}</span>
+            <p className="discount-price-detail">per {item.duration}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="price-box">
+          <p className="title-price">
+            <sup>Rp</sup>{" "}
+            <span>{formatPrice(item.price)}</span>
+            <p className="duration-price">per {item.duration}</p>
+          </p>
+        </div>
+      )
+
+    return (
+      <div key={key} className="item-price">
+        {recommendedStyle}
+        <h6>{item.hosting_type}</h6>
+        {discountPrice}
+        <div className="item-box-align">
+          <span className="item-right-align">{item.capacity_space}</span>
+          <span className="item-left-align">{item.capacity_type}</span>
+        </div>
+        <div className="item-box-align">
+          <span className="item-right-align">{item.bandwidth_capacity}</span>
+          <span className="item-left-align">{item.bandwidth_type}</span>
+        </div>
+        <div className="item-box-align">
+          <span className="item-right-align">{item.website_capacity}</span>
+          <span className="item-left-align">{item.website_type}</span>
+        </div>
+        <div className="item-box-align">
+          <span className="item-right-align">{item.email_capacity}</span>
+          <span className="item-left-align">{item.email_type}</span>
+        </div>
+        <div className="item-box-align">
+          <span className="item-right-align">{item.support_type}</span>
+          <span className="item-left-align">{item.support_capacity}</span>
+        </div>
+        <div
+          className={
+            item.free_domain && item.ssh_access
+              ? "extra-flexbox"
+              : "extra-flexbox-hidden"
+          }
+        >
+          <span className="text-center">Free Domain</span>
+          <span className="text-center">
+            SSH Access{" "}
+            <i class="far fa-question-circle">
+              <span className="domain-policy-tooltip">
+                {item.domain_policy}
+              </span>
+            </i>
+          </span>
+        </div>
+        <button className="price-btn">Order</button>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -23,71 +109,7 @@ function Price({data}) {
           <hr />
         </div>
         <div className="price-container">
-          {/* Mengambil data JSON kemudian menaruh ke dalam masing-masing item gridbox */}
-          {data.map((item, key) => {
-            return (
-              <div key={key} className="item-price">
-                <h6>{item.hosting_type}</h6>
-                <div className="price-box">
-                  <p className="title-price">
-                    <sup>Rp</sup>{" "}
-                    <span id="title-price-detail">
-                      {formatPrice(item.price)}
-                    </span>
-                  </p>
-                  <p className="duration-price">per {item.duration}</p>
-                </div>
-                <div className="item-box-align">
-                  <span className="item-right-align">
-                    {item.capacity_space}
-                  </span>
-                  <span className="item-left-align">{item.capacity_type}</span>
-                </div>
-                <div className="item-box-align">
-                  <span className="item-right-align">
-                    {item.bandwidth_capacity}
-                  </span>
-                  <span className="item-left-align">{item.bandwidth_type}</span>
-                </div>
-                <div className="item-box-align">
-                  <span className="item-right-align">
-                    {item.website_capacity}
-                  </span>
-                  <span className="item-left-align">{item.website_type}</span>
-                </div>
-                <div className="item-box-align">
-                  <span className="item-right-align">
-                    {item.email_capacity}
-                  </span>
-                  <span className="item-left-align">{item.email_type}</span>
-                </div>
-                <div className="item-box-align">
-                  <span className="item-right-align">{item.support_type}</span>
-                  <span className="item-left-align">
-                    {item.support_capacity}
-                  </span>
-                </div>
-                <div
-                  className={
-                    item.free_domain && item.ssh_access
-                      ? "extra-flexbox"
-                      : "extra-flexbox-hidden"
-                  }
-                >
-                  <span className="text-center">Free Domain</span>
-                  <span className="text-center">
-                    SSH Access{" "}
-                    <i class="far fa-question-circle">
-                      <span className="domain-policy-tooltip">
-                        {item.domain_policy}
-                      </span>
-                    </i>
-                  </span>
-                </div>
-                <button className="price-btn">Order</button>
-              </div>
-            );
-          })}
+          {infoPrice}
         </div>
       </section>
     </div>
